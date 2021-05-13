3a3c84b5d515aed8642f08f7f614f01f897e139e
johnmartins <johnmartins1992@gmail.com>
2021-05-13 15:53:57 +0200

    Additional fail-safes, and misc improvements

--- ansys.py
+++ ansys.py
@@ -14,9 +14,16 @@ job_dir_name = 'sed-job-ansys'
 mechanical_var_file = 'mechanical'
 geometry_var_file = 'geometry'
 results_file = 'results.txt'
+deformation_nodes_file = 'deformation_points.txt'
+stress_nodes_file = 'stress_points.txt'
 
 # Convenient variables
 job_path = fr'{tempfile.gettempdir()}\{job_dir_name}'
+
+
+def copy_if_exists(target, destination):
+    if os.path.exists(target):
+        shutil.copyfile(target, destination)
 
 
 def run_case(journal_file, mechanical_file, geometry_file, experiment_name, results_destination, verbose=False):
@@ -47,21 +54,40 @@ def run_case(journal_file, mechanical_file, geometry_file, experiment_name, resu
     if verbose:
         print(f'Turning over job to ANSYS workbench with journal: {journal_file}')
 
+    execution_failed = True
+
     try:
-        ansys_completed_process = subprocess.run(f'RunWB2.exe -R {journal_file}', shell=True, check=True, timeout=600)
+        ansys_completed_process = subprocess.run(f'RunWB2.exe -R {journal_file}', shell=True, check=True, timeout=1200)
         print(f'ANSYS workbench exited with code {str(ansys_completed_process.returncode)}')
+
+        if ansys_completed_process.returncode == 0:
+            # Even though we catch TimeoutExpired and CalledProcessError, there might be other unforeseen exceptions
+            # that may result in ansys failing to execute. So, we ensure that the executable returns 0 to be sure.
+            execution_failed = False
+
     except subprocess.TimeoutExpired:
-        print('This is taking forever! Pulling the plug.')
+        print('Timeout threshold exceeded. Pulling the plug on current case.')
+    except subprocess.CalledProcessError:
+        print('Non-zero response from ANSYS executable. This case should be investigated manually.')
+
+    if execution_failed:
+        results_destination += "/failed/"
+        print(f'Execution failed. Results (if any) will be stored in a separate directory: {results_destination}')
 
     # Offload results
     if results_destination is not None:
         results_destination_file_path = fr'{results_destination}\{experiment_name}.txt'
+        results_deformation_points_file_path = fr'{results_destination}\{experiment_name}-def.txt'
+        results_stress_points_file_path = fr'{results_destination}\{experiment_name}-str.txt'
 
         if verbose:
             print(f'Storing results in {results_destination_file_path}')
 
         Path(results_destination).mkdir(parents=True, exist_ok=True)
-        shutil.copyfile(fr'{job_path}\{results_file}', results_destination_file_path)
+
+        copy_if_exists(fr'{job_path}\{results_file}', results_destination_file_path)
+        copy_if_exists(fr'{job_path}\{deformation_nodes_file}', results_deformation_points_file_path)
+        copy_if_exists(fr'{job_path}\{stress_nodes_file}', results_stress_points_file_path)
 
     if verbose:
         print('Done.')
